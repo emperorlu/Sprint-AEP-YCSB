8da766c5c1b0c292a1a138df99754e63292dbe3d
emperorlu <1021779409@qq.com>
2019-11-08 15:57:02 +0800

    aep

--- run.sh
+++ run.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 ./rmsh
-numactl --cpunodebind=1 --membind=1 ./ycsbc -db aepsystem -dbpath /home/lk/ceshi -threads 1 -P workloads/1G_load.spec -load true -run true -dbstatistics true >> out.out
-
-./rmsh
-numactl --cpunodebind=1 --membind=1 ./ycsbc -db aepsystem -dbpath /home/lk/ceshi -threads 1 -P workloads/5G_load.spec -load true -run true -dbstatistics true >> out.out
-
-./rmsh
-numactl --cpunodebind=1 --membind=1 ./ycsbc -db aepsystem -dbpath /home/lk/ceshi -threads 1 -P workloads/10G_load.spec -load true -run true -dbstatistics true >> out.out
+numactl --cpunodebind=1 --membind=1 ./ycsbc -db aepsystem -dbpath /home/lk/ceshi -threads 1 -P workloads/20G_load.spec -load true -run true -dbstatistics true >> out.out
 
 ./rmsh
 numactl --cpunodebind=1 --membind=1 ./ycsbc -db aepsystem -dbpath /home/lk/ceshi -threads 1 -P workloads/15G_load.spec -load true -run true -dbstatistics true >> out.out
 
 ./rmsh
-numactl --cpunodebind=1 --membind=1 ./ycsbc -db aepsystem -dbpath /home/lk/ceshi -threads 1 -P workloads/20G_load.spec -load true -run true -dbstatistics true >> out.out
+numactl --cpunodebind=1 --membind=1 ./ycsbc -db aepsystem -dbpath /home/lk/ceshi -threads 1 -P workloads/10G_load.spec -load true -run true -dbstatistics true >> out.out
 
+./rmsh
+numactl --cpunodebind=1 --membind=1 ./ycsbc -db aepsystem -dbpath /home/lk/ceshi -threads 1 -P workloads/5G_load.spec -load true -run true -dbstatistics true >> out.out
+
+./rmsh
+numactl --cpunodebind=1 --membind=1 ./ycsbc -db aepsystem -dbpath /home/lk/ceshi -threads 1 -P workloads/1G_load.spec -load true -run true -dbstatistics true >> out.out
+
+./rmsh
 
