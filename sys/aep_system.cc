98a471a621a78f492039317966e89f5ece5519c1
emperorlu <1021779409@qq.com>
2019-11-08 15:24:07 +0800

    aep

--- sys/aep_system.cc
+++ sys/aep_system.cc
@@ -520,7 +520,7 @@ void aepsystem::Delete(const std::string& key)
 }
 
 aepsystem::aepsystem(){
-    is_cache = 0;
+    is_cache = 1;
     cache_size = 1;
     buf_size = KEY_SIZE + VALUE_SIZE + 1;
     // one = buf_size;
