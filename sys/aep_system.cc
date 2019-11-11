61e76d771fb0fed20c459e0a75d858641df076d6
emperorlu <1021779409@qq.com>
2019-11-11 10:38:24 +0800

    aep

--- sys/aep_system.cc
+++ sys/aep_system.cc
@@ -338,7 +338,7 @@ void aepsystem::Insert(const string &key, const string &value)
     m_mutex.lock();
     // std::lock_guard<std::mutex> lk(m_mutex);
     insert_count++;
-    cout << "[DEBUG] Insert (" << insert_count << ") key: " << key << endl;
+    // cout << "[DEBUG] Insert (" << insert_count << ") key: " << char8toint64(key.c_str()) << endl;
     if(id == 0)  // primary aep
     {
         bptree_nvm0->Insert(char8toint64(key.c_str()),value);
@@ -385,8 +385,8 @@ string aepsystem::Get(const std::string& key)
     m_mutex.lock();
     // std::lock_guard<std::mutex> lk(m_mutex);
     get_count++;
-    // cout << "[DEBUG] Get (" << get_count << ") key: " << char8toint64(key.c_str()) << " id: " << id << endl;
-    cout << "[DEBUG] Get (" << get_count << ") key: " << key << endl;
+    cout << "[DEBUG] Get (" << get_count << ") key: " << char8toint64(key.c_str()) << " id: " << id << endl;
+    // cout << "[DEBUG] Get (" << get_count << ") key: " << key << endl;
     if(id == 0)  // primary aep
     {
         tmp_value = bptree_nvm0->Get(char8toint64(key.c_str()));
