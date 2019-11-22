e9bbd7aa2d392cfd30669d183bb27f4bfe1f9596
emperorlu <1021779409@qq.com>
2019-11-22 10:41:17 +0800

    aep

--- db/db_factory.h
+++ db/db_factory.h
@@ -18,7 +18,7 @@ namespace ycsbc {
 
 class DBFactory {
  public:
-  static DB* CreateDB(utils::Properties &props);
+  static DB* CreateDB(utils::Properties &props, int to_cache, int num_size);
 };
 
 } // ycsbc
